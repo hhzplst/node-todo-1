7391b0cf553d6f1b99e044ddd1beb51a6c2da289
Elie Schoppik <eschoppik@gmail.com>
2015-09-01 12:00:59 -0700

    adding solution

--- index.js
+++ index.js
@@ -1,53 +1,66 @@
-var express = require("express");
-
-var bodyParser = require("body-parser");
-
-var app = express();
+var express = require("express"),
+    bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    app = express(),
+    morgan = require("morgan")
+    Todo = require("./todo");
 
 app.set("view engine", "ejs");
-
-var array = [], id=1, currentID, currentItem;
-
-function todoItem(name, id, link, memo){
-  this.name = name;
-  this.id = id;
-  this.link = link;
-  this.memo = memo;
-}
-
+app.use(morgan('tiny'));
+app.use(methodOverride("_method"));
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(express.static(__dirname + "/public/"));
 
-app.get("/todo", function(req, res){
+var array = [], id=1;
+
+app.get('/', function(req,res){
+  res.redirect("/todos")
+});
+
+app.get("/todos", function(req, res){
   res.render("index", {arr: array});
 });
 
-app.get("/todo/new", function(req, res){
+app.get("/todos/new", function(req, res){
   res.render("new");
 });
 
-app.post("/submit", function(req, res){
-  currentItem =  new todoItem(req.body.item, id, "http://localhost:3000/todo/"+id, "");
-  array.push(currentItem);
+app.post("/todos", function(req, res){
+  array.push(new Todo(req.body.item, id));
   id++;
-  res.render("index", {arr: array} );
+  res.redirect("/todos");
 });
 
-app.get("/todo/:id", function(req, res){
-  res.render("individual", {name: array[req.params.id-1].name, id: id-1, memo:array[req.params.id-1].memo});
-  currentID = req.params.id;
+app.get("/todos/:id", function(req, res){
+  var id = Number(req.params.id);
+  array.forEach(function(todo){
+    if(todo.id === id){
+      res.render("show", {todo:todo});
+    }
+  })
 });
 
-app.post("/add-memo", function(req, res){
-  array[currentID-1].memo = req.body.memo;
-  res.render("individual", {name: array[currentID-1].name, id: currentID-1, memo:array[currentID-1].memo});
+app.put("/todos/:id", function(req, res){
+  var id = Number(req.params.id);
+  array.forEach(function(todo){
+    if(todo.id === id){
+      todo.name = req.body.todo.name;
+      res.redirect("/todos")
+    }
+  })
 });
 
-app.post("/delete-item", function(req, res){
-  array[currentID-1] = "";
-  res.render("index", {arr: array});
+app.delete("/todos/:id", function(req, res){
+  var id = Number(req.params.id);
+  array.forEach(function(todo){
+    if(todo.id === id){
+      array.splice(array.indexOf(todo),1)
+      res.redirect("/todos")
+    }
+  })
 });
 
-app.listen(3000, function(){});
+app.listen(3000, function(){
+  console.log("server starting on port 3000")
+});
 
---
+++ todo.js
@@ -0,0 +1,5 @@
+module.exports = function TodoItem(name, id, link, memo){
+  this.name = name;
+  this.id = id;
+}
+
